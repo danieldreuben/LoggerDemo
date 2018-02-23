37c15e468f636028088cd7cd9cd45866e5b04adc
danieldreuben <daniel.reuben2@gmail.com>
2018-02-22 22:04:30 -0800

    test updates

--- src/main/java/com/threadcontextex/demo/DemoApplication.java
+++ src/main/java/com/threadcontextex/demo/DemoApplication.java
@@ -2,25 +2,10 @@ package com.threadcontextex.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-import org.apache.logging.log4j.ThreadContext;
 
 @SpringBootApplication
 public class DemoApplication {
-	private static final Logger logger = LogManager.getLogger(DemoApplication.class);
-
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
-    }
-
-    public DemoApplication() {
-    	ThreadContext.put("item-1","testing123");
-    	ThreadContext.put("item-2","testing345");
-    	ThreadContext.push("single");
-        logger.debug("This is a debug message");
-        logger.info("This is an info message");
-        logger.warn("This is a warn message");
-        logger.error("This is an error message");    	
     }
 }
--- src/test/java/com/threadcontextex/demo/DemoApplicationTests.java
+++ src/test/java/com/threadcontextex/demo/DemoApplicationTests.java
@@ -4,13 +4,33 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.ThreadContext;
+import java.util.Map;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class DemoApplicationTests {
+	private static final Logger logger = LogManager.getLogger(DemoApplication.class);
 
 	@Test
 	public void contextLoads() {
+	}
+
+	@Test
+	public void loggingTests() {
+		Map<String,String> mapCtx = Map.of("item", "shirt", "color", "blue","size","medium");
+		ThreadContext.putAll(mapCtx);
+
+		ThreadContext.put("item-1","testing123");
+    	ThreadContext.put("item-2","testing345");
+    	ThreadContext.push("single");
+        
+        logger.debug("Test:This is a debug message");
+        logger.info("Test:This is an info message");
+        logger.warn("Test:This is a warn message");
+        logger.error("Test:This is an error message");    
 	}
 
 }
